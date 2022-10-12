29eb8813aae5dc02bf529f21712b866c17aa2d1d
Maycon Fabio <m.vinnicyus@gmail.com>
2022-10-12 17:23:18 -0300

    feat: Add Heading component

--- src/components/Heading/index.stories.tsx
+++ src/components/Heading/index.stories.tsx
@@ -1,9 +1,9 @@
 import { Meta, StoryObj } from '@storybook/react'
-import { Text, TextProps } from '.'
+import { Heading, HeadingProps } from '.'
 
 export default {
-  title: 'Components/Text',
-  component: Text,
+  title: 'Components/Heading',
+  component: Heading,
   args: {
     children: 'Lorem ipsum',
     size: 'md',
@@ -15,26 +15,26 @@ export default {
       control: { type: 'inline-radio' },
     },
   },
-} as Meta<TextProps>
+} as Meta<HeadingProps>
 
-export const Default: StoryObj<TextProps> = {}
+export const Default: StoryObj<HeadingProps> = {}
 
-export const Small: StoryObj<TextProps> = {
+export const Small: StoryObj<HeadingProps> = {
   args: {
     size: 'sm',
   },
 }
 
-export const Large: StoryObj<TextProps> = {
+export const Large: StoryObj<HeadingProps> = {
   args: {
     size: 'lg',
   },
 }
 
-export const CustomComponent: StoryObj<TextProps> = {
+export const CustomComponent: StoryObj<HeadingProps> = {
   args: {
     asChild: true,
-    children: <p>Tag p</p>,
+    children: <h1>Tag h1</h1>,
   },
   argTypes: {
     children: { table: { disable: true } },
