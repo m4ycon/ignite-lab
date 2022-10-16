b304b8eb46098181047abce7f4a50ebf3fa30b47
Maycon Fabio <m.vinnicyus@gmail.com>
2022-10-16 20:11:25 -0300

    feat: Add complete page

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -6,7 +6,8 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
+    "@storybook/addon-a11y"
   ],
   "framework": "@storybook/react",
   "core": {
@@ -16,8 +17,8 @@ module.exports = {
     "storyStoreV7": true
   },
   viteFinal: (config, {configType}) => {
-    if (configType === 'PRODUCTION')
-      config.base = '/ignite-lab/'
+    if (configType === "PRODUCTION")
+      config.base = "/ignite-lab/"
 
     return config
   }
