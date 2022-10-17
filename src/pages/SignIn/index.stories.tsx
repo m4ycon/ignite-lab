943714f1980f01dd512f7bfb052c341657a429c1
Maycon Fabio <m.vinnicyus@gmail.com>
2022-10-16 21:55:44 -0300

    refactor: Create signin page

---
+++ src/pages/SignIn/index.stories.tsx
@@ -0,0 +1,11 @@
+import { Meta, StoryObj } from '@storybook/react'
+import { SignInPage } from '.'
+
+export default {
+  title: 'Pages/Sign in',
+  component: SignInPage,
+  args: {},
+  argTypes: {},
+}
+
+export const Default: StoryObj<{}> = {}
